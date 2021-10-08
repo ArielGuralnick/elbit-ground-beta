cb9c507366747e31613ccfca0653bfaa213401a1
Ariel Guralnick <86559715+ArielGuralnick@users.noreply.github.com>
2021-10-08 13:49:18 +0300

    priny ariel

--- main.py
+++ main.py
@@ -17,5 +17,7 @@ def errorsInSkyLark():
     if request.method == 'GET':
         return render_template('errorsInSkyLark.html')
 
+print("ariel")
+
 if __name__ == "__main__":
     app.run(debug=True)
