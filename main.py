605eaf10c89a51737c0d0c24e4dad1c6ad96bb16
Ariel Guralnick <86559715+ArielGuralnick@users.noreply.github.com>
2021-10-08 13:49:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def errorsInSkyLark():
     if request.method == 'GET':
         return render_template('errorsInSkyLark.html')
 
-print("ariel")
+print("itay")
 
 if __name__ == "__main__":
     app.run(debug=True)
