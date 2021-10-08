ab25e9498f22f506a4302007b28be06a06198b4b
Ariel Guralnick <86559715+ArielGuralnick@users.noreply.github.com>
2021-10-08 13:47:08 +0300

    Start project

---
+++ main.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from flask import Flask,render_template,request
+
+app = Flask(__name__)
+
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+@app.route("/skyLark", methods=['GET','POST'])
+def skyLark():
+    if request.method == 'GET':
+        return render_template('skyLark.html', title_simulator = "רוכב שמיים")
+
+@app.route("/skyLark/errorsInSkyLark", methods=['GET','POST'])
+def errorsInSkyLark():
+    if request.method == 'GET':
+        return render_template('errorsInSkyLark.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
