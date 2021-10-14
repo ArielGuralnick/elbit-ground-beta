a26b72d20f3c3e978f75c9ec01b799508aa3711c
Ariel Guralnick <86559715+ArielGuralnick@users.noreply.github.com>
2021-10-14 16:38:50 +0300

    upgrade

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import csv
+import pandas as pd
 from flask import Flask,render_template,request, redirect, flash,url_for
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '\xe0\\\x17\xb3\xca_\x82\x94\xf4\xa8w/;\x17&\xbbr\xf4;\xb6\x8f@\xcd\x7f'
+# for safety - we go to terminal, import os, then - os.urandom(12).hex(), and copy the result and paste here.
 
 @app.route("/", methods=['GET','POST'])
 def home():
@@ -12,15 +16,40 @@ def skyLark():
     if request.method == 'GET':
         return render_template('skyLark.html', title_simulator = "מאמן רוכב שמיים")
 
-header = ['תאריך', 'שעה', 'שם מזהה']
+@app.route("/activity", methods=['GET','POST'])
+def activity():
+    if request.method == 'GET':
+        return render_template('activity.html')
+    
+    elif request.method == 'POST':
+        position_upload = request.form.get('position_upload')
+        number_training = request.form.get('number_training')
+        date_upload = request.form.get('date_upload')
+        group_training = request.form.get('group_training')
+        name_updater = request.form.get('name_updater')
+        time_upload = request.form.get('time_upload')
+        name_downloader = request.form.get('name_downloader')
+        time_download = request.form.get('time_download')
+
+        field_content = ['סוג מאמן', 'מספר אימון', 'תאריך העלאה', 'פעילות עבור','שם המעלה', 'שעת התחלה', 'שם המורידה', 'שעת סיום']
+        with open('data_activity.csv', 'a', encoding='UTF8',newline='') as file:
+            writer = csv.DictWriter(file, fieldnames=field_content)
+#            writer.writeheader()
+            writer.writerow({'סוג מאמן': position_upload, 'מספר אימון' : number_training, 'תאריך העלאה' : date_upload,
+             'פעילות עבור' : group_training, 'שם המעלה' : name_updater,
+             'שעת התחלה' : time_upload, 'שם המורידה' : name_downloader, 'שעת סיום' : time_download})
+            flash(f'תיעוד האימון נקלט בהצלחה!', category="success")
+        return redirect(url_for('skyLark'))
+
+
 @app.route("/error", methods=['GET','POST'])
 def error():
     if request.method == 'GET':
         return render_template('error.html')
 
     elif request.method == 'POST':
-        date = request.form.get('date')
-        time = request.form.get('time')
+        date_error = request.form.get('date_error')
+        time_error = request.form.get('time_error')
         name_identifier = request.form.get('name_identifier')
         timing_fault = request.form.get('timing_fault')
         position = request.form.get('position')
@@ -31,16 +60,14 @@ def error():
         situation = request.form.get('situation')
         downtime = request.form.get('downtime')
 
-        field_content = ['date', 'time', 'name_identifier',
-         'timing_fault', 'position', 'type_of_fault', 'explanation',
-         'fault_operation', 'computer', 'situation', 'downtime']
+        field_content = ['תאריך', 'שעה', 'שם מזהה', 'עיתוי התקלה', 'עמדה', 'סוג התקלה', 'הסבר','תפעול התקלה', 'מחשב', 'טופל/לא טופל', 'זמן השבתה']
         with open('dataa.csv', 'a', encoding='UTF8',newline='') as file:
             writer = csv.DictWriter(file, fieldnames=field_content)
-            writer.writerow({'date' : date, 'time' : time, 'name_identifier':name_identifier,
-             'timing_fault': timing_fault, 'position' : position, 'type_of_fault' : type_of_fault,
-             'explanation' : explanation, 'fault_operation' : fault_operation,
-             'computer' : computer, 'situation' : situation, 'downtime' : downtime})
-#        flash('התקלה נקלטה בהצלחה!', category="success")
+#            writer.writeheader()
+            writer.writerow({'תאריך' : date_error, 'שעה' : time_error, 'שם מזהה':name_identifier, 'עיתוי התקלה': timing_fault, 'עמדה' : position, 'סוג התקלה' : type_of_fault,
+             'הסבר' : explanation, 'תפעול התקלה' : fault_operation,
+             'מחשב' : computer, 'טופל/לא טופל' : situation, 'זמן השבתה' : downtime})
+            flash(f'התקלה נקלטה בהצלחה!', category="success")
         return redirect(url_for('skyLark'))
 
 @app.route("/mars", methods=['GET','POST'])
