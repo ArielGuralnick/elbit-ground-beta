01ea23025541b31578583c125cdf72d39323392e
David Yair <Davismarsel@gmail.com>
2022-03-09 15:29:34 +0200

    EOL of unix

--- start_stack.sh
+++ start_stack.sh
@@ -1,1 +1,13 @@
-#!/bin/bash# Starting stackecho Starting stackecho Executing: docker-compose up -ddocker-compose up -decho ''
+#!/bin/bash
+
+
+
+# Starting stack
+
+
+echo Starting stack
+echo Executing: docker-compose up -d
+docker-compose up -d
+echo ''
+
+
