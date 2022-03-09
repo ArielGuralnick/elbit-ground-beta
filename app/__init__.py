78c8a758038503b832f27abbb6f428c5246c0e1a
David Yair <Davismarsel@gmail.com>
2022-03-09 13:48:28 +0200

    Added dependency pkg: aioflask==0.4.0 && Updated CD into root dir

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,11 @@
-import os
+import osimport pathlib
+
 from flask import Flask
 
 app = Flask(__name__)
 
-dir="C:/Users/E024478/Desktop/ProgrammsInPython/ElbitProj/Git"
-os.chdir(dir)
+rootDir=pathlib.Path().resolve()print("CD into dir:", rootDir)
+os.chdir(rootDir)
 
 # for safety - we go to terminal, import os, then - os.urandom(12).hex(), and copy the result and paste here.
 app.config['SECRET_KEY'] = '\xe0\\\x17\xb3\xca_\x82\x94\xf4\xa8w/;\x17&\xbbr\xf4;\xb6\x8f@\xcd\x7f'
