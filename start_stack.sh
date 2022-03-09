a2e77afe32262c873a94b90049fb7f21c3cf4c67
David Yair <Davismarsel@gmail.com>
2022-03-09 15:28:52 +0200

    Added 2 scripts for start & stop

---
+++ start_stack.sh
@@ -0,0 +1,1 @@
+#!/bin/bash# Starting stackecho Starting stackecho Executing: docker-compose up -ddocker-compose up -decho ''
---
+++ stop_stack.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+
+# Stopping stack
+
+
+echo Stopping stack
+echo Executing: docker-compose down
+docker-compose down
+echo ''
+
+
