938f235abd21c1f45e97329d608e553da321ab96
David Yair <Davismarsel@gmail.com>
2022-03-24 16:07:48 +0200

    Added default port

--- run.py
+++ run.py
@@ -3,16 +3,11 @@ import os
 import sys
 
 print("Reading env: 'PORT' for listening port assign")
-listen_port = os.getenv('PORT')
-print("Found PORT value:", listen_port)
-listen_port = 80
-if not listen_port:
-    print("Error - Missing env: 'PORT'")
-    print("Aborting..")
-    sys.exit(1)
+listen_port = os.getenv('PORT', 47382)
+
 
 if __name__ == "__main__":
-    app.run(debug=True, host="127.0.0.1", port=listen_port)  # Access from any IP - For linux containers..
-    # app.run(debug=True, host="0.0.0.0", port=listen_port)  # <- 127.0.0.1 == Access only from localhost
+    # app.run(debug=True, host="127.0.0.1", port=listen_port)  # Access from any IP - For linux containers..
+    app.run(debug=True, host="0.0.0.0", port=listen_port)  # <- 127.0.0.1 == Access only from localhost
 
 
