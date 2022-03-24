de7c3c3464061e652d022dd74654b9ac5bce3b8b
David Yair <Davismarsel@gmail.com>
2022-03-24 16:15:05 +0200

    Docs

--- run.py
+++ run.py
@@ -7,7 +7,7 @@ listen_port = os.getenv('PORT', 47382)
 
 
 if __name__ == "__main__":
-    # app.run(debug=True, host="127.0.0.1", port=listen_port)  # Access from any IP - For linux containers..
-    app.run(debug=True, host="0.0.0.0", port=listen_port)  # <- 127.0.0.1 == Access only from localhost
+    # app.run(debug=True, host="127.0.0.1", port=listen_port)  # <- 127.0.0.1 == Access only from localhost
+    app.run(debug=True, host="0.0.0.0", port=listen_port)  # Access from any IP - For linux containers / heruko
 
 
