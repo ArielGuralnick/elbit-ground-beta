d2d04c758b739c868ee3940dbdf12a818a6fa312
David Yair <Davismarsel@gmail.com>
2022-03-24 14:06:13 +0200

    Added listen_port = 80 for testing

--- run.py
+++ run.py
@@ -1,9 +1,18 @@
 from app import app
 import os
-import sysprint("Reading env: 'PORT' for listening port assign")listen_port = os.getenv('PORT')print("Found PORT value:", listen_port)if not listen_port:    print("Error - Missing env: 'PORT'")
-    print("Aborting..")    sys.exit(1)
+import sys
+
+print("Reading env: 'PORT' for listening port assign")
+listen_port = os.getenv('PORT')
+print("Found PORT value:", listen_port)
+listen_port = 80
+if not listen_port:
+    print("Error - Missing env: 'PORT'")
+    print("Aborting..")
+    sys.exit(1)
+
 if __name__ == "__main__":
-    # app.run(debug=True, host="127.0.0.1", port=47382)  # Access from any IP - For linux containers..
-    app.run(debug=True, host="0.0.0.0", port=listen_port)  # <- 127.0.0.1 == Access only from localhost
+    app.run(debug=True, host="127.0.0.1", port=listen_port)  # Access from any IP - For linux containers..
+    # app.run(debug=True, host="0.0.0.0", port=listen_port)  # <- 127.0.0.1 == Access only from localhost
 
 
