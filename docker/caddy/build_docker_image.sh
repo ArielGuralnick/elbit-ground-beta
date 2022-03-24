e0bd048c392e7ce1cb9329bbfb3c8ae921c40a4e
David Yair <Davismarsel@gmail.com>
2022-03-24 12:11:57 +0200

    Typo

--- docker/caddy/build_docker_image.sh
+++ docker/caddy/build_docker_image.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-echo Building imageDOCKER_BUILD_CMD="docker build -t caddy-2.4.2-alpine:latest . -f Dockerfile.alpine"
+echo Building imageexport DOCKER_BUILD_CMD="docker build -t caddy-2.4.2-alpine:latest . -f Dockerfile.alpine"
 echo Executing: $DOCKER_BUILD_CMD
-$($DOCKER_BUILD_CMD)
+eval $DOCKER_BUILD_CMD
 
