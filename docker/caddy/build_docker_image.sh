9c274e7f254b0b108c8d40468292a3a7cb027762
David Yair <Davismarsel@gmail.com>
2022-03-24 12:09:25 +0200

    Added arg: -f Dockerfile.alpine  to docker build cmnd

--- docker/caddy/build_docker_image.sh
+++ docker/caddy/build_docker_image.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-echo Building image:
-echo Executing: docker build -t caddy-2.4.2-alpine:latest .
-docker build -t caddy-2.4.2-alpine:latest .
+echo Building imageDOCKER_BUILD_CMD="docker build -t caddy-2.4.2-alpine:latest . -f Dockerfile.alpine"
+echo Executing: $DOCKER_BUILD_CMD
+eval $DOCKER_BUILD_CMD
 
