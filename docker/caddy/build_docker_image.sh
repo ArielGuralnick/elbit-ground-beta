16510eafa87ad836ab112914b7d821d3f33db6d7
David Yair <Davismarsel@gmail.com>
2022-03-24 12:10:59 +0200

    Build cmnd

--- docker/caddy/build_docker_image.sh
+++ docker/caddy/build_docker_image.sh
@@ -3,5 +3,5 @@
 
 echo Building imageDOCKER_BUILD_CMD="docker build -t caddy-2.4.2-alpine:latest . -f Dockerfile.alpine"
 echo Executing: $DOCKER_BUILD_CMD
-eval $DOCKER_BUILD_CMD
+$DOCKER_BUILD_CMD
 
