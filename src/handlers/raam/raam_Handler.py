f1261fb509f8e3ca6dedc7eeac82498ea094b2ef
Ariel Guralnick <86559715+ArielGuralnick@users.noreply.github.com>
2022-04-09 17:10:43 +0300

    Delete src/handlers/raam directory

--- src/handlers/raam/raam_Handler.py
+++
@@ -1,5 +0,0 @@
-from flask import render_template
-
-async def raam_Handler(request):
-    if request.method == 'GET':
-        return render_template('raam/raam.html', title_simulator = "עמדת טכנאי ומשובים", second_paragraph = "משפט כלשהו של רעם")
