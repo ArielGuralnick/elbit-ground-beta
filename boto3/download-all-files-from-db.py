6a42f2c61bf8ba3c75b296b5bb1c20a634f9c66f
David Yair <Davismarsel@gmail.com>
2022-05-23 15:19:13 +0300

    aws_bucket_name

--- boto3/download-all-files-from-db.py
+++ boto3/download-all-files-from-db.py
@@ -2,11 +2,13 @@
 import boto3
 import os
 
-bucket_name = "bucketeer-6a878c84-4c94-43bf-9c1c-7ef1bdebdc5c"
+aws_bucket_name = os.environ.get('AWS_BUCKET_NAME', "bucketeer-6a878c84-4c94-43bf-9c1c-7ef1bdebdc5c")
 
 aws_access_key_id = os.environ.get('AWS_ACCESS_KEY_ID', None)
 aws_secret_access_key = os.environ.get('AWS_SECRET_ACCESS_KEY', None)
 aws_bucket_region_name = os.environ.get('AWS_BUCKET_REGION_NAME', None)
+
+
 
 download_target_path = os.getcwd()
 
@@ -18,8 +20,8 @@ session = boto3.Session(
 print("Getting session to s3 bucket")
 s3 = session.resource('s3')
 
-print(f"Getting bucket: '{bucket_name}'")
-my_bucket = s3.Bucket(bucket_name)
+print(f"Getting bucket: '{aws_bucket_name}'")
+my_bucket = s3.Bucket(aws_bucket_name)
 
 # Create an S3 access object
 print(f"Getting client connection to s3")
@@ -68,7 +70,7 @@ def download_dir(prefix, local, bucket, client=s3_client):
 
 print("List all files to download")
 paginator = s3_client.get_paginator('list_objects')
-result = paginator.paginate(Bucket=bucket_name, Delimiter='/')
+result = paginator.paginate(Bucket=aws_bucket_name, Delimiter='/')
 for prefix in result.search('CommonPrefixes'):
     print(prefix.get('Prefix'))
 
@@ -77,7 +79,7 @@ print("Downloading all files")
 for prefix in result.search('CommonPrefixes'):
     dir_to_download = prefix.get('Prefix')
     print(f"Downloading: {dir_to_download}")
-    download_dir(dir_to_download, download_target_path, bucket_name, s3_client)
+    download_dir(dir_to_download, download_target_path, aws_bucket_name, s3_client)
 
 print("Finished")
 
