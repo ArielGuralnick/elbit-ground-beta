d32cdaa17fb925a6ad2b03a0b38486aa04a4b1cf
David Yair <Davismarsel@gmail.com>
2022-05-23 16:19:09 +0300

    print("Downloading all files from DB")

--- run.py
+++ run.py
@@ -7,7 +7,8 @@ print("Reading env: 'PORT' for listening port assign")
 listen_port = os.getenv('PORT', 47382)
 
 # Download all files from DB
-
+print("Downloading all files from DB")
+download_all_files_from_db.download_all_files_from_db()
 
 if __name__ == "__main__":
     # app.run(debug=True, host="127.0.0.1", port=listen_port)  # <- 127.0.0.1 == Access only from localhost
