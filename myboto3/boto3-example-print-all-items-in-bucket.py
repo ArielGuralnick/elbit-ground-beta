140f6dda1aa3e291464de641c41c27af8674afe0
Ariel Guralnick <86559715+ArielGuralnick@users.noreply.github.com>
2022-05-23 16:40:31 +0300

    Python boto3 aws (#3)
    * make it better
    
    * Added test boto3 file
    
    * Added idea
    
    * import boto3
    
    * Updated boto3 version to: 1.23.5
    
    * Added test with se bucket
    
    * aws_access_key_id = os.environ.get('AWS_ACCESS_KEY_ID', None)
    aws_secret_access_key = os.environ.get('AWS_SECRET_ACCESS_KEY', None)
    
    * Added AWS_BUCKET_REGION_NAME
    
    * Added file: boto3-example-download-items-from-bucket.py
    
    * Printings
    
    * Printings
    
    * Updated vars
    
    * Added example for downloading all files
    
    * Added download-all-files-from-db.py
    
    * aws_bucket_name
    
    * if None in [aws_access_key_id, aws_secret_access_key, aws_bucket_region_name]:
    
    * Updated all envs
    
    * Printings
    
    * Printings
    
    * gitpython==3.1.0
    
    * download_target_path = get_git_root(__file__)
    
    * download_target_path = os.path.join(git_root, 'app', 'db')
    
    * Spaces
    
    * typo
    
    * Updated vars
    
    * Updated set of required envs
    
    * Updated envs
    
    * Updated new module download-all-files-from-db.py
    
    * print("Downloading all files from DB")
    
    * Fixed typo
    
    * Updated download files on startup
    
    Co-authored-by: David Yair <Davismarsel@gmail.com>

---
+++ myboto3/boto3-example-print-all-items-in-bucket.py
@@ -0,0 +1,27 @@
+
+import boto3
+import os
+
+bucket_name = "bucketeer-6a878c84-4c94-43bf-9c1c-7ef1bdebdc5c"
+
+aws_access_key_id = os.environ.get('AWS_ACCESS_KEY_ID', None)
+aws_secret_access_key = os.environ.get('AWS_SECRET_ACCESS_KEY', None)
+aws_bucket_region_name = os.environ.get('AWS_REGION_NAME', None)
+
+session = boto3.Session(
+         aws_access_key_id=aws_access_key_id,
+         aws_secret_access_key=aws_secret_access_key)
+
+
+#Then use the session to get the resource
+print("Getting session to s3 bucket")
+s3 = session.resource('s3')
+
+print(f"Getting bucket: '{bucket_name}'")
+my_bucket = s3.Bucket(bucket_name)
+
+print("All items in bucket:")
+for i in my_bucket.objects.all():
+    print(i)
+
+
--- run.py
+++ run.py
@@ -1,12 +1,18 @@
 from app import app
+from myboto3 import download_files
 import os
 import sys
-import boto3
 
 
 print("Reading env: 'PORT' for listening port assign")
 listen_port = os.getenv('PORT', 47382)
 
+def download_updates_from_db():
+    # Download all files from DB
+    download_files.download_all_files_from_db()
+
 if __name__ == "__main__":
+    if os.environ.get('WERKZEUG_RUN_MAIN') != 'true':
+        download_updates_from_db()
     # app.run(debug=True, host="127.0.0.1", port=listen_port)  # <- 127.0.0.1 == Access only from localhost
     app.run(debug=True, host="0.0.0.0", port=listen_port)  # Access from any IP - For linux containers / heruko
